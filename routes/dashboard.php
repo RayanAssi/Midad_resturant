ab37851c0401dd99cb891305caee88c4a31151c6
Rama-abwdan <ramaabwdan06gmail.com>
2026-09-19 15:48:08 +0300

    edit admin order for filter and search and card

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -35,15 +35,6 @@ Route::middleware('auth:admin')
             Route::get('/{id}/edit', [OrdersController::class, 'edit'])->name('edit');
             Route::put('/{id}', [OrdersController::class, 'update'])->name('update');
             Route::delete('/{id}', [OrdersController::class, 'destroy'])->name('destroy');
-
-            Route::get('/{id}/calculate', [OrdersController::class, 'calculateInvoice'])->name('calculate');
-            Route::post('/{id}/invoice', [OrdersController::class, 'createInvoice'])->name('invoice');
-            Route::get('/filter/type/{type}', [OrdersController::class, 'filterByType'])->name('filter.type');
-            Route::get('/today/list', [OrdersController::class, 'todayOrders'])->name('today');
-            Route::get('/statistics/data', [OrdersController::class, 'statistics'])->name('statistics');
-            Route::get('/user/{userId}', [OrdersController::class, 'ordersByUser'])->name('by.user');
-            Route::get('/table/{tableNo}', [OrdersController::class, 'ordersByTable'])->name('by.table');
-            Route::post('/{id}/duplicate', [OrdersController::class, 'duplicateOrder'])->name('duplicate');
         });
 
         // Profile
