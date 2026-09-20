0d960979511d5d44159ffd3e618c28b36b7152c7
RayaNaSSi <rassi7266@gmail.com>
2026-09-20 21:59:29 +0300

    edit confirm password page

--- resources/views/cashier/auth/confirm-password.blade.php
+++ resources/views/cashier/auth/confirm-password.blade.php
@@ -1,18 +1,3 @@
-{{-- @php
-    $action = request('action', 'enable');
-    $isEnable = $action === 'enable';
-@endphp
-
-<x-profile.confirm-password 
-    :action="route('two-factor.enable')"
-    :method="$isEnable ? 'POST' : 'DELETE'"
-    title="Confirm Password"
-    :description="$isEnable 
-        ? 'Please confirm your password before enabling Two-Factor Authentication.' 
-        : 'Please confirm your password before disabling Two-Factor Authentication.'"
-    badge="Cashier Secure"
-    :logout-url="url('/cashier/logout')"
-/> --}}
 @php
     $action = route('password.confirm');
 @endphp
